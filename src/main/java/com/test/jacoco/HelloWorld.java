0f39a1f17ca0f2d94dfeb59cff7f0fcd2bf5b46a
luowebb <151220068@smail.nju.edu.cn>
2019-11-19 22:05:59 +0800

    finish assignment

--- src/main/java/com/test/jacoco/HelloWorld.java
+++ src/main/java/com/test/jacoco/HelloWorld.java
@@ -1,6 +1,7 @@
 package com.test.jacoco;
 
 import javax.imageio.event.IIOReadWarningListener;
+import java.util.Calendar;
 
 
 public class HelloWorld {
@@ -50,7 +51,12 @@ public class HelloWorld {
     	 * TODO: You need to complete this method to determine whether  a
     	 * triangle is formed or not when given the input edge a, b and c.
     	 */
-    	return true;
+    	int min = Math.min(a, Math.min(b, c));
+    	if (min <= 0)
+    		return false;
+    	int max = Math.max(a, Math.max(b, c));
+    	int mid = a + b + c - min - max;
+    	return max - min < mid;
     }
     public boolean isBirthday(int year, int month, int day) {
     	/**
@@ -58,7 +64,30 @@ public class HelloWorld {
     	 * legitimate date of birth between 1990/01/01 and 2019/10/01 is 
     	 * formed or not when given the input year, month and day.
     	 */
-    	return true;
+		Calendar begin = Calendar.getInstance();
+		Calendar end = Calendar.getInstance();
+		begin.clear();
+		begin.set(1990, 1 - 1, 1);
+		end.clear();
+		end.set(2019, 10 - 1, 1);
+
+		Calendar calendar = Calendar.getInstance();
+		calendar.clear();
+		calendar.set(year, month - 1, day);
+		calendar.setLenient(false);
+		try {
+			calendar.getTime();
+		} catch (Exception e) {
+			return false;
+		}
+
+		int r1 = calendar.compareTo(begin);
+		int r2 = calendar.compareTo(end);
+		if (r1 == 0 || r2 == 0)
+			return true;
+		if (r1 > 0 && r2 < 0)
+			return true;
+		return false;
     	
     }
     public Double miniCalculator(double a, double b, char op) {
@@ -67,8 +96,17 @@ public class HelloWorld {
     	 * can calculate the formula: "a op b", the op here can be four basic  
     	 * operation: "+","-","*","/". 
     	 */
-    	double result = 0.0;
-    	return result;
+		switch (op) {
+			case '+':
+				return a + b;
+			case '-':
+				return a - b;
+			case '*':
+				return a * b;
+			case '/':
+				return a / b;
+			default: throw new IllegalArgumentException();
+		}
     }
     
 }
--- src/test/java/com/test/jacoco/test/HelloWorldTest.java
+++ src/test/java/com/test/jacoco/test/HelloWorldTest.java
@@ -1,13 +1,13 @@
 package com.test.jacoco.test;
 
-import static org.junit.Assert.assertEquals;
-
 /**
  * Created by superZh on 2019/10/1.
  */
 
 import org.junit.Test;
 import com.test.jacoco.HelloWorld;
+
+        import static org.junit.Assert.*;
 
 public class HelloWorldTest {
     @Test
@@ -30,10 +30,83 @@ public class HelloWorldTest {
     	 * TODO：finish the test function
     	 */
     	HelloWorld hw = new HelloWorld();
-    	int a = hw.Method3(6, -1, 1);
-    	
+    	assertEquals(0, hw.Method3(6, -1, 1));
+        assertEquals(0, hw.Method3(6, 1, 1));
+    	assertEquals(0, hw.Method3(1, -1, 1));
+        assertEquals(0, hw.Method3(1, -1, -1));
+        assertEquals(0, hw.Method3(6, 1, -1));
+    	assertEquals(0, hw.Method3(1, -1, -1));
+        assertEquals(0, hw.Method3(1, -1, -3));
     }
     /**
      * TODO: add the test function of other methods in HelloWorld.java
      */
+    @Test
+    public void testMethod4() {
+        HelloWorld hw = new HelloWorld();
+        assertEquals(0, hw.Method4(0, 0, 0, 0, 0));
+        assertEquals(0, hw.Method4(1, 1, 0, 0, 0));
+        assertEquals(0, hw.Method4(5, 0, 0, 0, 0));
+
+        boolean thrown = false;
+        try {
+            hw.Method4(1, 0, 0, 0, 0);
+        } catch (ArithmeticException e) {
+            thrown = true;
+        }
+        assertTrue(thrown);
+
+        thrown = false;
+        try {
+            hw.Method4(1, 0, 0, 1, 0);
+        } catch (ArithmeticException e) {
+            thrown = true;
+        }
+        assertTrue(thrown);
+    }
+
+    @Test
+    public void testBug() {
+        HelloWorld hw = new HelloWorld();
+        assertTrue(hw.bug(5));
+        assertFalse(hw.bug(-5));
+    }
+
+    @Test
+    public void testIsTriangle() {
+        HelloWorld hw = new HelloWorld();
+        assertTrue(hw.isTriangle(3, 2, 2));
+        assertFalse(hw.isTriangle(-1, 3, 3));
+        assertFalse(hw.isTriangle(1, 1, 3));
+    }
+
+    @Test
+    public void testMiniCalculator() {
+        HelloWorld hw = new HelloWorld();
+        final double delta = 1e-6;
+        assertEquals(2, hw.miniCalculator(1, 1, '+'), delta);
+        assertEquals(3, hw.miniCalculator(1, 3, '*'), delta);
+        assertEquals(3, hw.miniCalculator(5, 2, '-'), delta);
+        assertEquals(2, hw.miniCalculator(4, 2, '/'), delta);
+
+        boolean thrown = false;
+        try {
+            hw.miniCalculator(1, 0, '%');
+        } catch (IllegalArgumentException e) {
+            thrown = true;
+        }
+        assertTrue(thrown);
+    }
+
+    @Test
+    public void testIsBirthday() {
+        HelloWorld hw = new HelloWorld();
+        assertFalse(hw.isBirthday(1980, 1, 1));
+        assertTrue(hw.isBirthday(1990, 1, 1));
+        assertTrue(hw.isBirthday(2009, 10, 1));
+        assertTrue(hw.isBirthday(2019, 10, 1));
+        assertFalse(hw.isBirthday(2029, 10, 1));
+
+        assertFalse(hw.isBirthday(2011, -1, -1));
+    }
 }
